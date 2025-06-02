3613c52e7fd5b0e273999e476c5acb9794e18046
codingashishdev <atankmember@gmail.com>
2025-06-02 09:14:51 +0530

    feat: add form submission handling and URL generation in script.js
    - Implemented form submission logic to generate a dynamic URL based on user input.
    - Added event listener to the form to handle submission and prevent default behavior.
    - Updated package.json to include jest for testing purposes.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // your code here
+const form = document.querySelector('#ourForm')
+const url = document.querySelector("#url")
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let name = form['name'].value
+    let year = form['year'].value
+    let submitUrl;
+    if(!name && !year) submitUrl = `https://localhost:8080/`
+    else if(name && year) submitUrl = `https://localhost:8080/?name=${name}&year=${year}`
+    else if(!name && year) submitUrl = `https://localhost:8080/?year=${year}`
+    else if(name && !year) submitUrl = `https://localhost:8080/?name=${name}`
+    url.innerHTML = submitUrl
+})
